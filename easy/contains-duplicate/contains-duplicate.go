504ea6c8a8b57e2f3e77856281536d98df78c561
Hieu Le Dinh Trung <hieu.ledt9@gmail.com>
2023-01-06 17:49:49 +0100

    Update

--- easy/contains-duplicate/contains-duplicate.go
+++ easy/contains-duplicate/contains-duplicate.go
@@ -4,7 +4,6 @@ func containsDuplicate(nums []int) bool {
 	// Using map as set to check the slice
 	checkMap := make(map[int]bool)
 
-
 	for _, num := range nums {
 
 		// If the key already exists, return true
